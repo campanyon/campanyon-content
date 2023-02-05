7fef0b4a897411981017f6466b713630ff394e17
Werner Huber <huberwerner@gmail.com>
2023-02-05 09:54:26 +0100

    add level to headings

--- schemas/objects/titleSection.js
+++ schemas/objects/titleSection.js
@@ -8,6 +8,14 @@ export default {
       type: 'string',
       title: 'Title',
     },
+    {
+      name: 'level',
+      type: 'number',
+      description: 'Value between 1 and 8 for heading levels',
+      title: 'Level (H1 - H8)',
+      initialValue: 2,
+      list: [1, 2, 3, 4, 5, 6, 7, 8]
+    },
   ],
   preview: {
     select: {
